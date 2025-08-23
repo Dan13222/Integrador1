77073e5aaba1a0db2f4f1d23e1154096cb19b510
danra <danra@192.168.1.11>
2025-08-23 10:50:38 -0600

    AVANCE 1: MENU PRINCIPAL DE USUARIO ADMINISTRADOR 23/08/2025  10:50 AM

---
+++ herramientasdedesarrollo/src/main/java/com/example/demo/HerramientasdedesarrolloApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerramientasdedesarrolloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerramientasdedesarrolloApplication.class, args);
+	}
+
+}
