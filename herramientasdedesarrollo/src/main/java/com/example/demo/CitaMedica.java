8c1b221c53ca7e174105e3e1d900a12e7a973fbf
danra <danra@192.168.1.6>
2025-10-15 17:52:16 -0600

    AVANCE 27

--- herramientasdedesarrollo/src/main/java/com/example/demo/CitaMedica.java
+++ herramientasdedesarrollo/src/main/java/com/example/demo/CitaMedica.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+
 public class CitaMedica {
 
 }
